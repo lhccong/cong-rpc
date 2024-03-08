6f844afe582c7949e758a7d2da9da68af1955c8a
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-03-08 16:38:51 +0800

    feat: 引入 Rpc 服务版本字段

--- cong-rpc-core/src/main/java/com/cong/rpc/core/constant/RpcConstant.java
+++ cong-rpc-core/src/main/java/com/cong/rpc/core/constant/RpcConstant.java
@@ -12,4 +12,8 @@ public interface RpcConstant {
    * 默认配置文件加载前缀
    */
   String DEFAULT_CONFIG_PREFIX = "rpc";
+  /**
+   * 默认服务版本
+   */
+  String DEFAULT_SERVICE_VERSION = "1.0";
 }
--- cong-rpc-core/src/main/java/com/cong/rpc/core/model/RpcRequest.java
+++ cong-rpc-core/src/main/java/com/cong/rpc/core/model/RpcRequest.java
@@ -1,5 +1,6 @@
 package com.cong.rpc.core.model;
 
+import com.cong.rpc.core.constant.RpcConstant;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,6 +29,11 @@ public class RpcRequest implements Serializable {
      * 方法名称
      */
     private String methodName;
+    /**
+     * 服务版本
+     */
+    private String serviceVersion = RpcConstant.DEFAULT_SERVICE_VERSION;
+
 
     /**
      * 参数类型列表
