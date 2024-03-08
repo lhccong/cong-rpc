e68b4e92570c0f0b58cf8700aeb2f415f6140c71
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-03-08 15:00:29 +0800

    perf: 将序列化工厂改成了使用单例双检索实现了懒加载

--- cong-rpc-core/src/main/java/com/cong/rpc/core/serializer/SerializerFactory.java
+++ cong-rpc-core/src/main/java/com/cong/rpc/core/serializer/SerializerFactory.java
@@ -1,13 +1,7 @@
 package com.cong.rpc.core.serializer;
 
-import com.cong.rpc.core.serializer.fastjson.FastJsonSerializer;
-import com.cong.rpc.core.serializer.hessian.HessianSerializer;
 import com.cong.rpc.core.serializer.jdk.JdkSerializer;
-import com.cong.rpc.core.serializer.kryo.KryoSerializer;
 import com.cong.rpc.core.spi.SpiLoader;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 序列化器工厂（用于获取序列化器对象）
@@ -16,14 +10,15 @@ import java.util.Map;
  * @date 2024/03/08
  */
 public class SerializerFactory {
+    /**
+     * 默认序列化器
+     */
+    private static  Serializer DEFAULT_SERIALIZER ;
     static {
         SpiLoader.load(Serializer.class);
     }
 
-    /**
-     * 默认序列化器
-     */
-    private static final Serializer DEFAULT_SERIALIZER = new JdkSerializer();
+
 
     /**
      * 获取实例
@@ -32,6 +27,14 @@ public class SerializerFactory {
      * @return {@link Serializer}
      */
     public static Serializer getInstance(String key) {
+        if (DEFAULT_SERIALIZER == null) {
+            synchronized (SerializerFactory.class) {
+                if (DEFAULT_SERIALIZER == null) {
+                    SpiLoader.load(Serializer.class);
+                    DEFAULT_SERIALIZER = new JdkSerializer();
+                }
+            }
+        }
         return SpiLoader.getInstance(Serializer.class, key);
     }
 
