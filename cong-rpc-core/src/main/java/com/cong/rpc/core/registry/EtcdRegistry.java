c9cc7ec82ebcbd74718acb280b3910ce4de07202
刘怀聪 <771901874@qq.com>
2024-03-08 20:44:23 +0800

    feat: 实现服务注册、注销、发现、注册中心销毁

--- cong-rpc-core/src/main/java/com/cong/rpc/core/registry/EtcdRegistry.java
+++ cong-rpc-core/src/main/java/com/cong/rpc/core/registry/EtcdRegistry.java
@@ -1,12 +1,16 @@
 package com.cong.rpc.core.registry;
 
+import cn.hutool.json.JSONUtil;
 import com.cong.rpc.core.config.RegistryConfig;
 import com.cong.rpc.core.model.ServiceMetaInfo;
-import io.etcd.jetcd.Client;
-import io.etcd.jetcd.KV;
+import io.etcd.jetcd.*;
+import io.etcd.jetcd.options.GetOption;
+import io.etcd.jetcd.options.PutOption;
 
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * etcd 注册表
@@ -14,7 +18,7 @@ import java.util.List;
  * @author cong
  * @date 2024/03/08
  */
-public class EtcdRegistry implements Registry  {
+public class EtcdRegistry implements Registry {
 
     private Client client;
 
@@ -33,21 +37,61 @@ public class EtcdRegistry implements Registry  {
 
     @Override
     public void register(ServiceMetaInfo serviceMetaInfo) throws Exception {
+        // 创建 Lease 和 KV 客户端
+        Lease leaseClient = client.getLeaseClient();
 
+        // 创建一个 30 秒的租约
+        long leaseId = leaseClient.grant(30).get().getID();
+
+        // 设置要存储的键值对
+        String registerKey = ETCD_ROOT_PATH + serviceMetaInfo.getServiceNodeKey();
+        ByteSequence key = ByteSequence.from(registerKey, StandardCharsets.UTF_8);
+        ByteSequence value = ByteSequence.from(JSONUtil.toJsonStr(serviceMetaInfo), StandardCharsets.UTF_8);
+
+        // 将键值对与租约关联起来，并设置过期时间
+        PutOption putOption = PutOption.builder().withLeaseId(leaseId).build();
+        kvClient.put(key, value, putOption).get();
     }
 
     @Override
     public void unRegister(ServiceMetaInfo serviceMetaInfo) {
-
+        kvClient.delete(ByteSequence.from(ETCD_ROOT_PATH + serviceMetaInfo.getServiceNodeKey(), StandardCharsets.UTF_8));
     }
 
     @Override
     public List<ServiceMetaInfo> serviceDiscovery(String serviceKey) {
-        return null;
+        //前缀搜索，结尾一定要加 '/'
+        String searchPrefix = ETCD_ROOT_PATH + serviceKey + "/";
+
+        try {
+            // 前缀查询
+            GetOption getOption = GetOption.builder().isPrefix(true).build();
+            List<KeyValue> keyValues = kvClient.get(
+                            ByteSequence.from(searchPrefix, StandardCharsets.UTF_8),
+                            getOption)
+                    .get()
+                    .getKvs();
+            // 解析服务信息
+            return keyValues.stream()
+                    .map(keyValue -> {
+                        String value = keyValue.getValue().toString(StandardCharsets.UTF_8);
+                        return JSONUtil.toBean(value, ServiceMetaInfo.class);
+                    })
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new RuntimeException("获取服务列表失败", e);
+        }
     }
 
     @Override
     public void destroy() {
-
+        System.out.println("当前节点下线");
+        // 释放资源
+        if (kvClient != null) {
+            kvClient.close();
+        }
+        if (client != null) {
+            client.close();
+        }
     }
 }
