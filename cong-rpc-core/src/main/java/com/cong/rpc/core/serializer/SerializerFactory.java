4e4130cf6cf39c0493b92788ee650928970646ed
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-03-08 16:03:01 +0800

    perf: 将序列化工厂改成了使用单例双检索实现了懒加载

--- cong-rpc-core/src/main/java/com/cong/rpc/core/serializer/SerializerFactory.java
+++ cong-rpc-core/src/main/java/com/cong/rpc/core/serializer/SerializerFactory.java
@@ -14,9 +14,7 @@ public class SerializerFactory {
      * 默认序列化器
      */
     private static  Serializer DEFAULT_SERIALIZER ;
-    static {
-        SpiLoader.load(Serializer.class);
-    }
+
 
 
 
