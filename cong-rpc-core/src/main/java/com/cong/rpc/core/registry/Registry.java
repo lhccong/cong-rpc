8f5480d48596f4dcf9013254510d1899561e9759
刘怀聪 <771901874@qq.com>
2024-03-08 20:01:30 +0800

    feat: 新增注册中心接口

---
+++ cong-rpc-core/src/main/java/com/cong/rpc/core/registry/Registry.java
@@ -0,0 +1,52 @@
+package com.cong.rpc.core.registry;
+
+
+import com.cong.rpc.core.config.RegistryConfig;
+import com.cong.rpc.core.model.ServiceMetaInfo;
+
+import java.util.List;
+
+/**
+ * 注册表
+ * 注册中心
+ *
+ * @author cong
+ * @date 2024/03/08
+ */
+public interface Registry {
+
+    /**
+     * 初始化
+     *
+     * @param registryConfig
+     */
+    void init(RegistryConfig registryConfig);
+
+    /**
+     * 注册服务（服务端）
+     *
+     * @param serviceMetaInfo 服务元信息
+     * @throws Exception 例外
+     */
+    void register(ServiceMetaInfo serviceMetaInfo) throws Exception;
+
+    /**
+     * 注销服务（服务端）
+     *
+     * @param serviceMetaInfo
+     */
+    void unRegister(ServiceMetaInfo serviceMetaInfo);
+
+    /**
+     * 服务发现（获取某服务的所有节点，消费端）
+     *
+     * @param serviceKey 服务键名
+     * @return
+     */
+    List<ServiceMetaInfo> serviceDiscovery(String serviceKey);
+
+    /**
+     * 服务销毁
+     */
+    void destroy();
+}
