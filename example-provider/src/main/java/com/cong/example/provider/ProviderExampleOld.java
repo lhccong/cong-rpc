03e902fb3bf8cb6b12ee06ac9a4bbaab5553b9bf
刘怀聪 <771901874@qq.com>
2024-03-10 13:36:43 +0800

    fix: 修改服务提供者示例代码

--- example-provider/src/main/java/com/cong/example/provider/ProviderExampleOld.java
+++ example-provider/src/main/java/com/cong/example/provider/ProviderExampleOld.java
@@ -9,16 +9,17 @@ import com.cong.rpc.core.model.ServiceMetaInfo;
 import com.cong.rpc.core.registry.LocalRegistry;
 import com.cong.rpc.core.registry.Registry;
 import com.cong.rpc.core.registry.RegistryFactory;
-import com.cong.rpc.core.server.tcp.VertxTcpServer;
+import com.cong.rpc.core.server.http.HttpServer;
+import com.cong.rpc.core.server.VertxHttpServer;
 
 /**
  * 提供程序示例
  * 服务提供者示例
  *
- * @author 86188
- * @date 2024/03/10
+ * @author cong
+ * @date 2024/03/08
  */
-public class ProviderExample {
+public class ProviderExampleOld {
 
     public static void main(String[] args) {
         // RPC 框架初始化
@@ -34,16 +35,16 @@ public class ProviderExample {
         Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
         ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
         serviceMetaInfo.setServiceName(serviceName);
-        serviceMetaInfo.setServiceHost(rpcConfig.getServerHost());
         serviceMetaInfo.setServicePort(rpcConfig.getServerPort());
+        serviceMetaInfo.setServiceHost(rpcConfig.getServerHost());
         try {
             registry.register(serviceMetaInfo);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-        // 启动 TCP 服务
-        VertxTcpServer vertxTcpServer = new VertxTcpServer();
-        vertxTcpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
+        // 启动 web 服务
+        HttpServer httpServer = new VertxHttpServer();
+        httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
     }
 }
