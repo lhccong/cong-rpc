9f1e1d400578e4c12e38d8002f00bfa9725bd8c1
刘怀聪 <771901874@qq.com>
2024-03-10 16:23:38 +0800

    fix: 修改 VertxTcpServer 代码，处理请求使用升级后的 TcpServerHandler

--- cong-rpc-core/src/main/java/com/cong/rpc/core/server/tcp/VertxTcpServer.java
+++ cong-rpc-core/src/main/java/com/cong/rpc/core/server/tcp/VertxTcpServer.java
@@ -1,11 +1,8 @@
 package com.cong.rpc.core.server.tcp;
 
 import com.cong.rpc.core.server.http.HttpServer;
-import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetServer;
-import io.vertx.core.parsetools.RecordParser;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -26,41 +23,11 @@ public class VertxTcpServer implements HttpServer {
         NetServer server = vertx.createNetServer();
 
         // 处理请求
-        server.connectHandler(socket -> {
-            // 构造 parser
-            RecordParser parser = RecordParser.newFixed(8);
-            parser.setOutput(new Handler<Buffer>() {
-                // 初始化
-                int size = -1;
-                // 一次完整的读取（头 + 体）
-                Buffer resultBuffer = Buffer.buffer();
-
-                @Override
-                public void handle(Buffer buffer) {
-                    if (-1 == size) {
-                        // 读取消息体长度
-                        size = buffer.getInt(4);
-                        parser.fixedSizeMode(size);
-                        // 写入头信息到结果
-                        resultBuffer.appendBuffer(buffer);
-                    } else {
-                        // 写入体信息到结果
-                        resultBuffer.appendBuffer(buffer);
-                        System.out.println(resultBuffer.toString());
-                        // 重置一轮
-                        parser.fixedSizeMode(8);
-                        size = -1;
-                        resultBuffer = Buffer.buffer();
-                    }
-                }
-            });
-
-            socket.handler(parser);
-        });
-
+        server.connectHandler(new TcpServerHandler());
         // 启动 TCP 服务器并监听指定端口
         server.listen(port, result -> {
             if (result.succeeded()) {
+                System.out.println("TCP server started on port " + port);
                 log.info("TCP server started on port " + port);
             } else {
                 log.info("Failed to start TCP server: " + result.cause());
