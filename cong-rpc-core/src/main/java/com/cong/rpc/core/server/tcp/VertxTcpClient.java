1493ca215e7460cce3f4316aaba24666b12a43ea
刘怀聪 <771901874@qq.com>
2024-03-10 16:23:04 +0800

    fix: 修改 VertxTcpClient 代码，接收响应调用 TcpBufferHandlerWrapper

--- cong-rpc-core/src/main/java/com/cong/rpc/core/server/tcp/VertxTcpClient.java
+++ cong-rpc-core/src/main/java/com/cong/rpc/core/server/tcp/VertxTcpClient.java
@@ -1,12 +1,90 @@
 package com.cong.rpc.core.server.tcp;
 
+import cn.hutool.core.util.IdUtil;
+import com.cong.rpc.core.RpcApplication;
+import com.cong.rpc.core.constant.ProtocolConstant;
+import com.cong.rpc.core.model.RpcRequest;
+import com.cong.rpc.core.model.RpcResponse;
+import com.cong.rpc.core.model.ServiceMetaInfo;
+import com.cong.rpc.core.protocol.*;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.NetClient;
+import io.vertx.core.net.NetSocket;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author cong
  */
 public class VertxTcpClient {
+
+    /**
+     * 做请求
+     * 发送请求
+     *
+     * @param rpcRequest      RPC 请求
+     * @param serviceMetaInfo 服务元信息
+     * @return {@link RpcResponse}
+     * @throws InterruptedException 中断异常
+     * @throws ExecutionException
+     */
+    public static RpcResponse doRequest(RpcRequest rpcRequest, ServiceMetaInfo serviceMetaInfo) throws InterruptedException, ExecutionException {
+        // 发送 TCP 请求
+        Vertx vertx = Vertx.vertx();
+        NetClient netClient = vertx.createNetClient();
+        CompletableFuture<RpcResponse> responseFuture = new CompletableFuture<>();
+        netClient.connect(serviceMetaInfo.getServicePort(), serviceMetaInfo.getServiceHost(),
+                result -> {
+                    if (!result.succeeded()) {
+                        System.err.println("Failed to connect to TCP server");
+                        return;
+                    }
+                    NetSocket socket = result.result();
+                    // 发送数据
+                    // 构造消息
+                    ProtocolMessage<RpcRequest> protocolMessage = new ProtocolMessage<>();
+                    ProtocolMessage.Header header = new ProtocolMessage.Header();
+                    header.setMagic(ProtocolConstant.PROTOCOL_MAGIC);
+                    header.setVersion(ProtocolConstant.PROTOCOL_VERSION);
+                    header.setSerializer((byte) ProtocolMessageSerializerEnum.getEnumByValue(RpcApplication.getRpcConfig().getSerializer()).getKey());
+                    header.setType((byte) ProtocolMessageTypeEnum.REQUEST.getKey());
+                    // 生成全局请求 ID
+                    header.setRequestId(IdUtil.getSnowflakeNextId());
+                    protocolMessage.setHeader(header);
+                    protocolMessage.setBody(rpcRequest);
+
+                    // 编码请求
+                    try {
+                        Buffer encodeBuffer = ProtocolMessageEncoder.encode(protocolMessage);
+                        socket.write(encodeBuffer);
+                    } catch (IOException e) {
+                        throw new RuntimeException("协议消息编码错误");
+                    }
+
+                    // 接收响应
+                    TcpBufferHandlerWrapper bufferHandlerWrapper = new TcpBufferHandlerWrapper(
+                            buffer -> {
+                                try {
+                                    ProtocolMessage<RpcResponse> rpcResponseProtocolMessage =
+                                            (ProtocolMessage<RpcResponse>) ProtocolMessageDecoder.decode(buffer);
+                                    responseFuture.complete(rpcResponseProtocolMessage.getBody());
+                                } catch (IOException e) {
+                                    throw new RuntimeException("协议消息解码错误");
+                                }
+                            }
+                    );
+                    socket.handler(bufferHandlerWrapper);
+
+                });
+
+        RpcResponse rpcResponse = responseFuture.get();
+        // 记得关闭连接
+        netClient.close();
+        return rpcResponse;
+    }
 
     public void start() {
         // 创建 Vert.x 实例
