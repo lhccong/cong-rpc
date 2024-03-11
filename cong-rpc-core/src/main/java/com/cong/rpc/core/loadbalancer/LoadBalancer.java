063d132ef8be2a78c575c502549ad2976d2a402f
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-03-11 14:07:03 +0800

    feat: 新增负载均衡接口

---
+++ cong-rpc-core/src/main/java/com/cong/rpc/core/loadbalancer/LoadBalancer.java
@@ -0,0 +1,23 @@
+package com.cong.rpc.core.loadbalancer;
+
+
+import com.cong.rpc.core.model.ServiceMetaInfo;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 负载均衡器（消费端使用）
+ * @author liuhuaicong
+ */
+public interface LoadBalancer {
+
+    /**
+     * 选择服务调用
+     *
+     * @param requestParams       请求参数
+     * @param serviceMetaInfoList 可用服务列表
+     * @return {@link ServiceMetaInfo}
+     */
+    ServiceMetaInfo select(Map<String, Object> requestParams, List<ServiceMetaInfo> serviceMetaInfoList);
+}
