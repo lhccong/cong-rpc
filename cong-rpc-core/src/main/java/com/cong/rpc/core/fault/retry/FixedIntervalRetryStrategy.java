a5ae31e8a26931764e3f66323e44e35396dcafb4
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-03-12 10:38:31 +0800

    feat: 新增固定时间间隔 - 重试策略

---
+++ cong-rpc-core/src/main/java/com/cong/rpc/core/fault/retry/FixedIntervalRetryStrategy.java
@@ -0,0 +1,43 @@
+package com.cong.rpc.core.fault.retry;
+
+import com.cong.rpc.core.model.RpcResponse;
+import com.github.rholder.retry.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 固定时间间隔 - 重试策略
+ *
+ * @author cong
+ * @date 2024/03/12
+ */
+@Slf4j
+public class FixedIntervalRetryStrategy implements RetryStrategy {
+
+    /**
+     * 重试
+     *
+     * @param callable 调用
+     * @return {@link RpcResponse}
+     * @throws ExecutionException 执行异常
+     * @throws RetryException     重试异常
+     */
+    public RpcResponse doRetry(Callable<RpcResponse> callable) throws ExecutionException, RetryException {
+        Retryer<RpcResponse> retryer = RetryerBuilder.<RpcResponse>newBuilder()
+                .retryIfExceptionOfType(Exception.class)
+                .withWaitStrategy(WaitStrategies.fixedWait(3L, TimeUnit.SECONDS))
+                .withStopStrategy(StopStrategies.stopAfterAttempt(3))
+                .withRetryListener(new RetryListener() {
+                    @Override
+                    public <V> void onRetry(Attempt<V> attempt) {
+                        log.info("重试次数 {}", attempt.getAttemptNumber());
+                    }
+                })
+                .build();
+        return retryer.call(callable);
+    }
+
+}
