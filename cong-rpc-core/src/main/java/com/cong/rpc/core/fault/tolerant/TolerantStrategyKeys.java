47ddc14c378659839621cfb9f36433c63508fcbc
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-03-13 15:58:55 +0800

    feat: 引入容错策略键名常量

---
+++ cong-rpc-core/src/main/java/com/cong/rpc/core/fault/tolerant/TolerantStrategyKeys.java
@@ -0,0 +1,31 @@
+package com.cong.rpc.core.fault.tolerant;
+
+/**
+ * 容错策略键名常量
+ *
+ * @author cong
+ * @date 2024/03/13
+ */
+public interface TolerantStrategyKeys {
+
+    /**
+     * 故障恢复
+     */
+    String FAIL_BACK = "failBack";
+
+    /**
+     * 快速失败
+     */
+    String FAIL_FAST = "failFast";
+
+    /**
+     * 故障转移
+     */
+    String FAIL_OVER = "failOver";
+
+    /**
+     * 静默处理
+     */
+    String FAIL_SAFE = "failSafe";
+
+}
