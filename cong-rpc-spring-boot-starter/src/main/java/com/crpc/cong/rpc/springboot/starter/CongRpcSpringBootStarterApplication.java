bdeea806592d133d5f5151a76446f1a0a8b704a8
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-03-14 10:49:43 +0800

    feat: 初始化 cong-rpc-springboot-starter

---
+++ cong-rpc-spring-boot-starter/src/main/java/com/crpc/cong/rpc/springboot/starter/CongRpcSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.crpc.cong.rpc.springboot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongRpcSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CongRpcSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ cong-rpc-spring-boot-starter/src/test/java/com/crpc/cong/rpc/springboot/starter/CongRpcSpringBootStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crpc.cong.rpc.springboot.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+class CongRpcSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("111");
+    }
+
+}
