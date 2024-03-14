903091e0bfbe2de4127d6cffb7b833ea7d34dc9e
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-03-14 10:52:05 +0800

    fix: 修改 ProviderExample 引入 ProviderBootstrap 初始化

--- example-provider/src/main/java/com/cong/example/provider/ProviderExample.java
+++ example-provider/src/main/java/com/cong/example/provider/ProviderExample.java
@@ -2,14 +2,11 @@ package com.cong.example.provider;
 
 
 import com.cong.example.service.UserService;
-import com.cong.rpc.core.RpcApplication;
-import com.cong.rpc.core.config.RegistryConfig;
-import com.cong.rpc.core.config.RpcConfig;
-import com.cong.rpc.core.model.ServiceMetaInfo;
-import com.cong.rpc.core.registry.LocalRegistry;
-import com.cong.rpc.core.registry.Registry;
-import com.cong.rpc.core.registry.RegistryFactory;
-import com.cong.rpc.core.server.tcp.VertxTcpServer;
+import com.cong.rpc.core.bootstrap.ProviderBootstrap;
+import com.cong.rpc.core.model.ServiceRegisterInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 提供程序示例
@@ -21,29 +18,12 @@ import com.cong.rpc.core.server.tcp.VertxTcpServer;
 public class ProviderExample {
 
     public static void main(String[] args) {
-        // RPC 框架初始化
-        RpcApplication.init();
+        // 要注册的服务
+        List<ServiceRegisterInfo<?>> serviceRegisterInfoList = new ArrayList<>();
+        ServiceRegisterInfo serviceRegisterInfo = new ServiceRegisterInfo(UserService.class.getName(), UserServiceImpl.class);
+        serviceRegisterInfoList.add(serviceRegisterInfo);
 
-        // 注册服务
-        String serviceName = UserService.class.getName();
-        LocalRegistry.register(serviceName, UserServiceImpl.class);
-
-        // 注册服务到注册中心
-        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
-        RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
-        Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
-        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
-        serviceMetaInfo.setServiceName(serviceName);
-        serviceMetaInfo.setServiceHost(rpcConfig.getServerHost());
-        serviceMetaInfo.setServicePort(rpcConfig.getServerPort());
-        try {
-            registry.register(serviceMetaInfo);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        // 启动 TCP 服务
-        VertxTcpServer vertxTcpServer = new VertxTcpServer();
-        vertxTcpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
+        // 服务提供者初始化
+        ProviderBootstrap.init(serviceRegisterInfoList);
     }
 }
