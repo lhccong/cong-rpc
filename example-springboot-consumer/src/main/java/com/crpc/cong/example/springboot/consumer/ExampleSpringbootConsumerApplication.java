a23fbbede98229cf2b4c254091a639b5f437dffd
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-03-14 11:36:23 +0800

    feat: 实现 ExampleSpringbootConsumer 使用 SpringBoot 项目初始化

---
+++ example-springboot-consumer/src/main/java/com/crpc/cong/example/springboot/consumer/ExampleSpringbootConsumerApplication.java
@@ -0,0 +1,19 @@
+package com.crpc.cong.example.springboot.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 示例 springboot consumer 应用程序
+ *
+ * @author cong
+ * @date 2024/03/14
+ */
+@SpringBootApplication
+public class ExampleSpringbootConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSpringbootConsumerApplication.class, args);
+    }
+
+}
