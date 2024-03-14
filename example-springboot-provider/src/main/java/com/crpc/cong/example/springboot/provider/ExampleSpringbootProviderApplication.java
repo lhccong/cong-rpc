3a4910674afc2531094b74fa4d5a31dac0bc3e9b
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-03-14 11:33:01 +0800

    feat: 实现 ExampleSpringbootProvider 使用 SpringBoot 项目初始化

---
+++ example-springboot-provider/src/main/java/com/crpc/cong/example/springboot/provider/ExampleSpringbootProviderApplication.java
@@ -0,0 +1,19 @@
+package com.crpc.cong.example.springboot.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 示例springboot提供程序应用程序
+ *
+ * @author cong
+ * @date 2024/03/14
+ */
+@SpringBootApplication
+public class ExampleSpringbootProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSpringbootProviderApplication.class, args);
+    }
+
+}
